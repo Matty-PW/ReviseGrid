089b163771c0aa2d7ca44240c7d1edf9a07c433a
matty <mattyprinkuwright@gmail.com>
2026-09-11 22:35:40 +0100

    add base app widget template

---
+++ RevisionWidget/RevisionWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  RevisionWidgetBundle.swift
+//  RevisionWidget
+//
+//  Created by Matty on 11/09/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RevisionWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RevisionWidget()
+        RevisionWidgetControl()
+    }
+}
